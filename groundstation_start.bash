267882e9addda292138b1cdf7522ed5df97f6418
MckennaCisler <mckennacisler@gmail.com>
2018-06-04 20:47:24 -0400

    Write system startup service files

---
+++ groundstation_start.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# startup command for groundstation
+# used in system service
+python ./groundstation.py
---
+++ install.bash
@@ -0,0 +1,9 @@
+# installs the EQUiSat groundstation on a RPi system
+
+# systemctl startup service
+cp ./equistation.service /usr/lib/systemd/user/
+sudo systemctl enable equistation.service
+sudo systemctl start equistation.service
+
+echo "control the system service with:"
+echo "'sudo systemctl start|stop|status equistation.service'"
